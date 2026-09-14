e723ba2310a310fdae263a4d9c16979db16a1eb9
jerrymarth10-del <jerrymarth10@gmail.com>
2026-09-14 08:17:30 -0400

    feat: add store inventory admin route

---
+++ app/admin/estoque/page.js
@@ -0,0 +1,9 @@
+import StoreInventoryAdmin from '@/components/store-inventory-admin';
+
+export const metadata = {
+  title: 'Estoque por loja | Casa do Pão de Queijo'
+};
+
+export default function StoreInventoryPage() {
+  return <StoreInventoryAdmin />;
+}
